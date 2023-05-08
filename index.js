8d55f0df538331594be13a179d02f4d384bc796a
aminulhaque98 <aminulhaque419@gmail.com>
2023-05-08 20:12:10 +0530

    some Modify

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -19,13 +19,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function linkland() {
     try {
         const socialPostCollection = client.db('linklandSocial').collection('socialPosts');
+        const socialCmntCollection = client.db('linklandSocial').collection('socialComment');
 
         //Create posts
         app.post('/socialPosts', async (req, res) => {
             const post = req.body;
             const result = await socialPostCollection.insertOne(post);
             res.send(result);
-            console.log(result)
         });
 
         //data get
@@ -33,7 +33,36 @@ async function linkland() {
             const query = {};
             const posts = await socialPostCollection.find(query).toArray();
             res.send(posts);
+        });
+
+        //reactUloading
+        // app.patch('/socialPosts/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const reacting = req.body.reacting;
+        //     const query = { _id: ObjectId(id) };
+        //     const updatedDoc = {
+        //         $set: {
+        //             reacting: reacting
+        //         }
+        //     };
+        //     const result = await socialPostCollection.updateOne(query, updatedDoc);
+        //     res.sent(result);
+        // });
+
+        //Create post comment
+        app.post('/socialComment', async (req, res) => {
+            const comment = req.body;
+            const result = await socialCmntCollection.insertOne(comment);
+            res.send(result);
         })
+
+        //get socialComment
+        app.get('/socialComment', async (req, res) => {
+            const query = {};
+            const comments = await socialCmntCollection.find(query).toArray();
+            res.send(comments);
+        });
+
 
     }
     finally {
